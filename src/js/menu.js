8ec0ed8c436ef2ab553bf1a7bed2ac25d69f175b
benavern <bencrdc@gmail.com>
2016-08-02 01:00:14 +0200

    layout + mobile menu + home stuff

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,7 +2,11 @@ var menu = angular.module('dashboard.menu', [])
 
 menu.controller('navCtrl', ['$scope', '$location', function($scope, $location) {
     $scope.isActive = function(path) {
-        return $location.path() == path;
+        return $location.path() == path
+    }
+
+    $scope.toggleMenu = function() {
+        document.body.classList.toggle('menu-visible')
     }
 }])
 
